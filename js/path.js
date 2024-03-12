46df5650012c9e2fd2612e88550e37d2ab176104
Joinside <43420583+Joinsider@users.noreply.github.com>
2024-03-12 17:05:21 +0000

    First Implementation of the Djikstra Algorithm for shortest Path search -- No Testing
    Signed-off-by: GitHub <noreply@github.com>

--- js/path.js
+++ js/path.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const pathSubmit = document.getElementById("pathSubmit");
-  const startingPoint = document.getElementById("StartingPointInput").value;
-  const endPoint = document.getElementById("EndPointInput").value;
+  let startingPoint = document.getElementById("StartingPointInput").value;
+  let endPoint = document.getElementById("EndPointInput").value;
   const pathDiv = document.getElementById("pathDiv");
   let graphElement = new Graph();
   let weightedGraph = new Djikstra();
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const loadKey = "GraphSave";
 
   pathSubmit.addEventListener("click", function () {
+    startingPoint = document.getElementById("StartingPointInput").value;
+    endPoint = document.getElementById("EndPointInput").value;
     let path = weightedGraph.shortestDist(loadKey, startingPoint, endPoint);
     console.log("Djikstra RUN!")
     console.log(path);
